4ab19b02937978b92023917bb421c6727d35f5ae
Victory Deo <victory23tshimbadi@gmail.com>
2024-08-03 10:59:06 +0100

    add login screen

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,47 @@
+'use client';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main>
+      <form>
+        <div>
+          <h1>Connexion</h1>
+          <p>Content de vous revoir</p>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="johndoe@gmail.com"
+            />
+            <p>{"L'adresse email doit contenir un @"}</p>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <Link
+                href="/forgetPassword"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                Mot de passe oublié
+              </Link>
+            </div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="********"
+            />
+          </div>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default page;
